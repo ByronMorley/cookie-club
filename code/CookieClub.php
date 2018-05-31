562f839f61f8b11f44631bb29fe6872430cf9135
Byron <byron@atebol.com>
2018-05-31 21:01:37 +0100

    update

--- code/CookieClub.php
+++ code/CookieClub.php
@@ -5,8 +5,10 @@ class CookieClub extends DataExtension {
 	public function contentControllerInit()
 	{
 		Requirements::css(COOKIE_CLUB_DIR . '/css/style.min.css');
-		Requirements::css(COOKIE_CLUB_DIR . '/ext/bootstrap/css/bootstrap.min.css');
+		Requirements::css('twitter/bootstrap/dist/css/bootstrap.min.css');
 
+		Requirements::javascript('components/jquery/jquery.min.js');
+		Requirements::javascript('twitter/bootstrap/dist/js/bootstrap.min.js');
 		Requirements::javascript(COOKIE_CLUB_DIR . '/js/main.js');
 	}
 
--- templates/Includes/Cookie.ss
+++ templates/Includes/Cookie.ss
@@ -3,9 +3,6 @@
 	document.body.className = document.body.className + " js_enabled";
 </script>
 
-<script type="text/javascript" src="$ModuleDir/ext/jquery/jquery.js"></script>
-<script type="text/javascript" src="$ModuleDir/ext/bootstrap/js/bootstrap.min.js"></script>
-
 <!-- The Modal -->
 <div id="cookie-message" data-cookie-expiry="60" class="modal fade" role="dialog">
 	<div class="modal-dialog">
